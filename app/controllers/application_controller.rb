c9b4ad87e2cb85ab438b424d83aac3e70cafb77f
emlee9017 <elee18@chicagobooth.edu>
2015-11-24 12:55:50 -0600

    HW 8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,14 @@ class ApplicationController < ActionController::Base
 
   #Require sign-in first
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+protected
+
+def configure_permitted_parameters
+  devise_parameter_sanitizer.for(:sign_up) << :username
+  devise_parameter_sanitizer.for(:sign_up) << :avatar_url
+
+  devise_parameter_sanitizer.for(:account_update) << :avatar_url
+end
 end
